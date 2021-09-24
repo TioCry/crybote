100eb97185e498cf2db06d88a16783d2cd03ef26
TioCry <89874775+TioCry@users.noreply.github.com>
2021-09-25 01:05:15 +0200

    Add files via upload

---
+++ help.js
@@ -0,0 +1,9 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+               message.channel.send('bot under developement, wait for me to add commands.');
+}
+
+module.exports.help = {
+  name:"help"
+}
